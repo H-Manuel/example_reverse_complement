d9ee0f21530def4e223caafac1727ec271ce39a2
H-Manuel <hm2799@mci4me.at>
2024-11-07 11:57:23 +0100

    added impelementation of reverse_complement function

--- reverse_complement.py
+++ reverse_complement.py
@@ -1,3 +1,24 @@
 """
 This script should provide the function "reverse_complement" that takes a DNA sequence as input and returns the reverse complement of the sequence.
 """
+
+
+def reverse_complement(dna_sequence: str)->str:
+    output=[]
+    for i in dna_sequence:
+        if (i == "A"):
+            output.append("T")
+        if (i == "T"):
+            output.append("A")
+        if (i == "G"):
+            output.append("C")
+        if (i == "C"):
+            output.append("G")
+
+    output.reverse()
+  
+
+    string=''.join(output)        
+    return string
+
+    # python -m unittest discover test
